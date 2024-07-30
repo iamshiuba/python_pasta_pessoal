01afd86b55109110f13c728a71eaf94a17028146
sekiun-san <168146145+sekiun-san@users.noreply.github.com>
2024-07-30 17:15:16 -0300

    Create: calculo_media

--- psw_4.0_aquecimento/index.py
+++ psw_4.0_aquecimento/index.py
@@ -1,31 +1,33 @@
+def calcular_media(notas):
+    return sum(notas) / len(notas)
+
+
 nota1 = float(input("Digite a nota 1: "))
 nota2 = float(input("Digite a nota 2: "))
 nota3 = float(input("Digite a nota 3: "))
 nota4 = float(input("Digite a nota 4: "))
 
-media = (nota1 + nota2 + nota3 + nota4) / 4
+notas = [nota1, nota2, nota3, nota4]
+media = calcular_media(notas)
 
-if media < 6:
+if media < 5:
+    if media >= 4.5 and nota4 < 5.5:
+        print("Recuperação!")
+    elif media < 4.5 and nota4 < 8:
+        print("Recuperação mediante a risco de reprovação por insuficiência!")
+
     posRec = float(input("Digite a sua nota da recuperação: "))
+    notas[3] = posRec
+    notaFinal = calcular_media(notas)
+else:
+    notaFinal = media
 
-mediaInicial = 6
-
-notaFinal = (nota1 + nota2 + nota3 + posRec) / 4 if media < 6 else media
-
-if media >= 6:
+if notaFinal >= 5:
     print("Aprovado!")
-elif mediaInicial >= 5.5 and mediaInicial < 6 and nota4 < 6.5:
-    print("Recuperação!")
-elif mediaInicial <= 5.5 and mediaInicial < 6 and nota4 < 9:
-    print("Recuperação mediante a risco de reprovação por insuficiência!")
-elif notaFinal >= mediaInicial:
-    print("O resultado da recuperação foi positivo! Aprovado!")
 else:
     print("O resultado da recuperação foi negativo! Reprovado!")
 
-if media >= 6:
-    print(f"Sua nota global foi: {nota1 + nota2 + nota3 + nota4}, média: {media}")
-elif notaFinal >= mediaInicial:
-    print(f"Sua nota global foi: {nota1 + nota2 + nota3 + posRec}, média: {notaFinal}")
+if media >= 5:
+    print(f"Sua nota global foi: {sum(notas)}, média: {media:.2f}")
 else:
-    print(f"Sua nota global foi: {nota1 + nota2 + nota3 + posRec}, média: {notaFinal}")
+    print(f"Sua nota global foi: {sum(notas)}, média: {notaFinal:.2f}")
